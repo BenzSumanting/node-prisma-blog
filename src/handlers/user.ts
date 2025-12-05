1bdc6047beae03d052f6f8f839d1f5ef62dcc79b
Benz-mtechlab <benzsuma@mtechlabs.dev>
2025-12-05 19:29:37 +0800

    install prisma and migrations

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
 import { CreateUserDto } from "../dtos/CreateUser.dto";
+import prisma from "../lib/prisma";
+import { User } from "../generated/prisma/client";
 
 export class UserHandler {
-  getUsers = (request: Request, response: Response) => {
-    response.send([]);
+  getUsers = async (request: Request, response: Response) => {
+    const users = await prisma.user.findMany();
+    response.status(200).json({success:"true", data:users})
   };
 
   getUsersById = (request: Request, response: Response) => {
